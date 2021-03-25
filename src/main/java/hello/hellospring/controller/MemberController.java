cfb45d24f26137ab5ffff7130c8c8d2439845717
Sean-Creative <ksw6125000@naver.com>
2021-03-26 00:43:07 +0900

    ..

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 //멤버 컨트롤러를 만들어야 하는데, 멤버 컨트롤러가 멤버 서비스를 통해서 데이터를
 //        조회할 수 있어야 한다. 이런 관계를 -> 서로 의존관계가 있다고 표현한다.
@@ -17,6 +18,10 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
     //객체를 따로 만들필요 없이, 스프링 컨테이너 한테 등록하면 -> 딱 하나만 등록이 된다.
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 }
