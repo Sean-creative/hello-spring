f3ee3a76be329eb1cefff6cd0479df5ef0f03ce0
Sean-Creative <ksw6125000@naver.com>
2021-03-28 23:25:41 +0900

    ..

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,19 @@
 package hello.hellospring.domain;
 
+
+import javax.persistence.*;
+
+@Entity
+//Member는 jap가 관리하는 엔티티가 된다.
 public class Member {
 
+
+    @Id //pk라는 뜻
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB에다가 값을 넣지 않아도 자동으로 생성해주는 값을 - IDENTITY
     private Long id;
+
+    //이러한 어노테이션을 가지고 DB와 매핑하는 것이다.
+    @Column(name = "username")
     private String name;
 
 
