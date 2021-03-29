f05723673ee868753f633091258cd7286da474b3
Sean-Creative <ksw6125000@naver.com>
2021-03-29 19:06:27 +0900

    ..

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,28 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+//AOP는 Aspect라고 써줘야 한다.
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    //패키지 하위에는 다 적용하라는 뜻
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+
+
+        }
+    }
+}
--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -8,6 +8,9 @@ import java.util.Optional;
 //스프링 데이터 jpa가 MemberRepository를 받고 있으면 얘가 구현체를 자동으로 빈으로 등록해준다
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
-    @Override
+
+    //select m from Member m where m.name = ?
+    //인터페이스 이름만으로도 개발이 끝났다다
+   @Override
     Optional<Member> findByName(String name);
 }
